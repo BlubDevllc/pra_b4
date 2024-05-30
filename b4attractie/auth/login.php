39c7b3f6aa1a13eeffd2364cdaf9b5fd4adbea39
d290805 <135005260+d290805@users.noreply.github.com>
2024-05-30 11:32:45 +0200

    login form
    login form gemaakt

--- b4attractie/auth/login.php
+++ b4attractie/auth/login.php
@@ -8,6 +8,21 @@ include_once "../components/head.php";
     ?>
 
     <main>
+    <div class="login">
+            <form action="backend/loginController.php" method="post" class="login">
+                <label for="login-username">Gebruikersnaam:</label>
+                <input type="text" id="login-username" name="username" placeholder="Gebruikersnaam" required>
+                <label for="login-password">Wachtwoord:</label>
+                <?php 
+                if(isset($_GET['msg']))
+                {
+                    echo "<p class='error'>" . $_GET['msg'] . "</p>";
+                }
+                ?>
+                <input type="password" id="login-password" name="password" placeholder="Wachtwoord" required>
+                <input type="submit" id="login" value="login" name="submit">
+            </form>
+        </div>
 
     </main>
 
