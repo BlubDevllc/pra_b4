b4ecf1d8fc714600bfac0d85724cda41aa63e202
dek0039 <85032105+MarkEcza@users.noreply.github.com>
2024-06-03 11:25:04 +0200

    Change concept of db creation

--- private/createDatabase.php
+++ private/createDatabase.php
@@ -4,26 +4,14 @@ include_once '../config/conn.php';
 
 function createDatabase($conn)
 {
-    $query = "CREATE TABLE upcoming_attractions (
+    $query = "CREATE TABLE current_attractions (
         id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
         type varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
         capacity int NOT NULL,
         openes_on date DEFAULT NULL,
         extra_information varchar(255) NOT NULL,
-        status varchar(255) NOT NULL
-      )";
-
-    $statement = $conn->prepare($query);
-    $statement->execute([]);
-    
-    $query = "CREATE TABLE current_attractions (
-        id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-        type varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-        capacity int NOT NULL,
-        opened_on date DEFAULT NULL,
-        extra_information varchar(255) NOT NULL,
+        photo_name varchar(255) NOT NULL,
         status varchar(255) NOT NULL
       )";
 
