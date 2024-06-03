0c024d44d75d19a842538e0037d81d3b9369813e
dek0039 <85032105+MarkEcza@users.noreply.github.com>
2024-06-03 11:10:23 +0200

    Database creation file

---
+++ private/createDatabase.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once '../config/conn.php';
+
+function createDatabase($conn)
+{
+    $query = "CREATE TABLE upcoming_attractions (
+        id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+        type varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+        capacity int NOT NULL,
+        openes_on date DEFAULT NULL,
+        extra_information varchar(255) NOT NULL,
+        status varchar(255) NOT NULL
+      )";
+
+    $statement = $conn->prepare($query);
+    $statement->execute([]);
+    
+    $query = "CREATE TABLE current_attractions (
+        id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+        type varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+        capacity int NOT NULL,
+        opened_on date DEFAULT NULL,
+        extra_information varchar(255) NOT NULL,
+        status varchar(255) NOT NULL
+      )";
+
+
+    $statement = $conn->prepare($query);
+    $statement->execute([]);
+
+    $query = "CREATE TABLE admins (
+        id int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        username varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+        password varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+        email varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+      )";
+
+    $statement = $conn->prepare($query);
+    $statement->execute([]);
+}
+
+
+createDatabase($conn);
