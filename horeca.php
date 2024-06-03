fbf563726663151a7455af072a34160fe2d81ad6
dave de visser <blubdev.llc@gmail.com>
2024-06-03 13:28:46 +0200

    horeca.php style update
    ik heb wat kleine style dingetjes toe gevoegd voor een mooier effect!

--- horeca.php
+++ horeca.php
@@ -9,45 +9,67 @@ include_once "components/head.php";
     <div class="ph">
     <div class="ph1">
         <button type="button" class="collapsible">the dogg</button>
-        <div class="content">
-            <p>this a little fast food chain here in developerland</p>
+            <div class="content">
+                <img class="dogg" src="assets\TheDogg.jpg" alt="">
+                <p>The Dogg is een food stand met verschillende soorten hotdogs en drankjes</p>
         
-        </div>
+            </div>
         </div>
     <div class='ph2'>
 
     <button type="button" class="collapsible">The bakery</button>
         <div class="content">
-            <p>test</p>
+            
+            <img class="bakery" src="assets\TheBakery.webp" alt="">        
+            <p>The Bakery is een klein en schattig bakerijtje met allerlij soorten gebak en banket die all uw zoetekauw benodig heden heeft</p>
+        </div>
     </div>
-    </div>
-    <div class='ph3'>
-        <button type="button" class="collapsible">kios</button>
+        <div class='ph3'>
+        <button type="button" class="collapsible">kiosk</button>
         <div class="content">
-            <p>test</p>
+            <img class="kiosk" src="assets\kiosk.jpg" alt="">
+                <p>de kiosk is een klein wikeltje voor all uw benodig heden tijdens het </p>
+        </div>
     </div>
-    </div>
-    <div class='ph4'>   
+        <div class='ph4'>   
 
+        
         <button type="button" class="collapsible">phat Jimmy's</button>
-        <div class="content">
-            <p>test</p>
+            <div class="content">
+                <p>test</p>
+            </div>
+        </div>
+        <div class='ph5'>   
+
+        
+            <button type="button" class="collapsible">phat Jimmy's</button>
+            <div class="content">
+                <p>test</p>
+            </div>
+        </div>
+        <div class='ph6'>   
+
+        
+            <button type="button" class="collapsible">phat Jimmy's</button>
+            <div class="content">
+                <p>test</p>
+            </div>
+        </div>
     </div>
-    </div
-    </div>
+
 </body>
 <script>
-    var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
