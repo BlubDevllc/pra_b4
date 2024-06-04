42dbb36728168fbd210899044c6458e2f59b28c0
d290805 <135005260+d290805@users.noreply.github.com>
2024-06-04 15:22:23 +0200

    login path
    login path in de header gezet

--- components/header.php
+++ components/header.php
@@ -8,7 +8,7 @@
                 <a href="">Ondek Ons</a>
                 <a href="/pra_b4/attractie/">Attracties</a>
                 <a href="/pra_b4/attractie/create.php">Submit Attractie</a>
-                <a href="">Inloggen</a>
+                <a href="/pra_b4/auth/login.php">Inloggen</a>
                 <a href="/pra_b4/auth/register.php">Registreren</a>
                 <a href="horeca.php">Horeca</a>
             </nav>
