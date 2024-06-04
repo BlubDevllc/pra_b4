54a1b611ae569aee2fecabd845bc5387bccde348
JoanMGH <106810822+JoanMGH@users.noreply.github.com>
2024-06-04 16:01:10 +0200

    front end register gedaan

--- auth/register.php
+++ auth/register.php
@@ -22,26 +22,29 @@ include_once "../components/head.php";
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         }
         ?>
-
+        <div class="login-form">
         <form action="../backend/registerController.php" method="POST">
         <div class="form-group">
                 <label for="user">Naam:</label>
-                <input type="text" name="user" id="user" placeholder="naam">
+                <input type="text" name="user" id="user" placeholder="naam" class="input-register">
             </div>
             <div class="form-group">
                 <label for="user">Email:</label>
-                <input type="email" name="email" id="email" placeholder="email">
+                <input type="email" name="email" id="email" placeholder="email" class="input-register">
             </div>
             <div class="form-group">
                 <label for="password">Wachtwoord:</label>
-                <input type="password" name="password" id="password" placeholder="pass">
+                <input type="password" name="password" id="password" placeholder="pass" class="input-register">
             </div>
             <div class="form-group">
                 <label for="password_check">Wachtwoord herhalen:</label>
-                <input type="password" name="password_check" id="password_check" placeholder="pass">
+                <input type="password" name="password_check" id="password_check" placeholder="pass" class="input-register">
             </div>
-            <input type="submit" value="Registreren">
+            <div class="form-group">
+                <input type="submit" value="Registreren">
+            </div>
         </form>
+        </div>
     </div>
 
     <?php
