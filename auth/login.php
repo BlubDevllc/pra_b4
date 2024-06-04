b3a00f967a46b528a95a76f9b33f188f445f205c
d290805 <135005260+d290805@users.noreply.github.com>
2024-06-04 15:28:28 +0200

    dubbele code verwerkt
    alle dubbele code eruit gehaald

--- auth/login.php
+++ auth/login.php
@@ -10,46 +10,30 @@ include_once "../components/head.php";
     <main>
         <div class="wrapper">
             <div class="center">
-               
+                <h1>login</h1>
+        <       <div class="login">
+                    <form action="backend/loginController.php" method="post" class="login">
+                        <label for="login-username">Gebruikersnaam:</label>
+                        <input type="text" id="login-username" name="username" placeholder="Gebruikersnaam" required>
+
+                        <label for="login-password">Wachtwoord:</label>
+                        <?php 
+                        if(isset($_GET['msg']))
+                        {
+                            echo "<p class='error'>" . $_GET['msg'] . "</p>";
+                        }
+                        ?>
+                        <input type="password" id="login-password" name="password" placeholder="Wachtwoord" required>
+                
+                        <input type="submit" id="login" value="login" name="submit">
+                    </form>
+                </div>
             </div>
         </div>
     </main>
-
-    <?php
-    include_once "../components/footer.php";
-    ?>
-<?php
-include_once "../components/head.php";
-?>
-
-<body>
-    <?php
-    include_once "../components/header.php";
-    ?>
-
-    <main>
-        <h1>login</h1>
-        <div class="login">
-            <form action="backend/loginController.php" method="post" class="login">
-                <label for="login-username">Gebruikersnaam:</label>
-                <input type="text" id="login-username" name="username" placeholder="Gebruikersnaam" required>
-
-                <label for="login-password">Wachtwoord:</label>
-                <?php 
-                if(isset($_GET['msg']))
-                {
-                    echo "<p class='error'>" . $_GET['msg'] . "</p>";
-                }
-                ?>
-                <input type="password" id="login-password" name="password" placeholder="Wachtwoord" required>
-                
-                <input type="submit" id="login" value="login" name="submit">
-            </form>
-        </div>
-
-    </main>
-
-    <?php
-    include_once "../components/footer.php";
-    ?>
+    <footer>
+        <?php
+        include_once "../components/footer.php";
+        ?>
+    </footer>
 </body>
