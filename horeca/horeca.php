13e2a199891c5d50980bfe90466b1341cf102a37
dave de visser <blubdev.llc@gmail.com>
2024-06-06 10:03:31 +0200

    horeca finaly change
    horeca.php is klaar ik moest de wacht tijd nog even toe voegen en de pathings goed zetten

--- horeca/horeca.php
+++ horeca/horeca.php
@@ -1,23 +1,24 @@
 <?php
+
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php?msg=Je bent niet ingelogd");
+    header("Location: auth/login.php?msg=Je bent niet ingelogd");
     exit;
-}
- 
-include_once "components/head.php";
+ }
+
+include_once "../components/head.php";
 ?>
 
 <body>
     <?php
-    include_once "components/header.php";
+    include_once "../components/header.php";
     ?>
     <div class="ph">
     <div class="ph1">
         <button type="button" class="collapsible">the dogg</button>
             <div class="content">
-                <img class="dogg" src="assets\TheDogg.jpg" alt="">
+                <img class="dogg" src="../assets\TheDogg.jpg" alt="">
                 <p>The Dogg is een food stand met verschillende soorten hotdogs en drankjes</p>
-        
+                <p class="wacht">de wachttijd is ongeveer 10 minuten</p>
             </div>
         </div>
     <div class='ph2'>
@@ -25,22 +26,25 @@ include_once "components/head.php";
     <button type="button" class="collapsible">The bakery</button>
         <div class="content">
             
-            <img class="bakery" src="assets\TheBakery.webp" alt="">        
+            <img class="bakery" src="../assets\TheBakery.webp" alt="">        
             <p>The Bakery is een klein en schattig bakerijtje met allerlij soorten gebak en banket die all uw zoetekauw benodig heden heeft</p>
+            <p class="wacht">de wachttijd is ongeveer 30 minuten</p>
         </div>
     </div>
         <div class='ph3'>
         <button type="button" class="collapsible">kiosk</button>
         <div class="content">
-            <img class="kiosk" src="assets\kiosk.jpg" alt="">
+            <img class="kiosk" src="../assets\kiosk.jpg" alt="">
                 <p>de kiosk is een klein wikeltje voor all uw benodig heden tijdens het </p>
-        </div>
+                <p class="wacht">de wachttijd is ongeveer 2 minuten</p>
+            </div>
     </div>
         <div class='ph4'>   
         <button type="button" class="collapsible">phat Jimmy's</button>
             <div class="content">
-                <img class="phat" src="assets/phat.jpg" alt="">
+                <img class="phat" src="../assets/phat.jpg" alt="">
                 <p>phat jimmy's is een burger tent met allerlij soorten en maten burgers perfect voor een lange dag in het park!</p>
+                <p class="wacht">de wachttijd is ongeveer 15 minuten</p>
             </div>
         </div>
         <div class='ph5'>   
@@ -48,8 +52,9 @@ include_once "components/head.php";
         
             <button type="button" class="collapsible">slush rush</button>
             <div class="content">
-                <img class="slush" src="assets/slush.jpg" alt="">
+                <img class="slush" src="../assets/slush.jpg" alt="">
                 <p>slush rush is een slushy tent voor als het dagje uit je net iets te warm word!</p>
+                <p class='wacht'>de wachttijd is ongeveer 5 minuten</p>
             </div>
         </div>
         <div class='ph6'>   
@@ -57,8 +62,9 @@ include_once "components/head.php";
         
             <button type="button" class="collapsible">pitstsa</button>
             <div class="content">
-                <img class="pizza" src="assets/pizza.jpg" alt="">
+                <img class="pizza" src="../assets/pizza.jpg" alt="">
                 <p>pitstsa is een kleine pizza tent waar pizza's op de orgineele itiliaanse manier in een heete steen over </p>
+                <p class='wacht'>de wachttijd is ongeveer 20 minuten</p>
             </div>
         </div>
     </div>
